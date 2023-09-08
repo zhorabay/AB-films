064ad8d3c9ecb5076175118343b206d7d1228791
bhushan354 <deshmukhumesh354@gmail.com>
2023-09-08 15:29:28 +0530

    linters fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import displayItems from './module/displayItems.js';
 import './module/addPopupOne.js';
 import './module/allCommentsOne.js';
 import './module/commentsCounter.js';
-import { addCommentPopupEvent } from './module/displayPopup.js';
+import addCommentPopupEvent from './module/displayPopup.js';
 
 await displayItems();
 
